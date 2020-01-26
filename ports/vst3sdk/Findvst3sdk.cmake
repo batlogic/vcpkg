53c6b883d3bbdea35ad9ebceb6e099581a56fdd9
Michael Mattsson <michael.mattsson@konicaminolta.com>
2020-01-26 18:35:36 -0500

    Initial checking of the vst3sdk port

---
+++ ports/vst3sdk/Findvst3sdk.cmake
@@ -0,0 +1,60 @@
+message(" Looking for vst3sdk! ")
+if(WIN32)
+else(WIN32)
+  message(FATAL_ERROR "Findvst3sdk.cmake: Unsupported platform ${CMAKE_SYSTEM_NAME}" )
+endif(WIN32)
+
+find_path(
+  VST3SDK_HOME
+  vst3sdk
+)
+
+if (NOT "${VST3SDK_HOME}" STREQUAL "")
+  set(VST3SDK_HOME
+	${VST3SDK_HOME}/vst3sdk
+  )
+endif()
+
+
+message(" Findvst3sdk VST3SDK_HOME : ${VST3SDK_HOME}")
+
+find_path(VST3SDK_INCLUDE_DIR
+  vst3stdsdk.cpp
+  PATHS
+  ${VST3SDK_HOME}/public.sdk/source 
+)  
+
+
+if (NOT "${VST3SDK_HOME}" STREQUAL "")
+	set (VST3SDK_INCLUDE_DIR
+		${VST3SDK_HOME}/
+		${VST3SDK_HOME}/base/source
+		${VST3SDK_HOME}/base/thread/include
+		${VST3SDK_HOME}/base/thread/source
+	
+		${VST3SDK_HOME}/pluginterfaces/base
+		${VST3SDK_HOME}/pluginterfaces/gui
+		${VST3SDK_HOME}/pluginterfaces/vst
+		${VST3SDK_HOME}/public.sdk
+		${VST3SDK_HOME}/public.sdk/source
+		${VST3SDK_HOME}/public.sdk/source/common
+		${VST3SDK_HOME}/public.sdk/source/main
+		${VST3SDK_HOME}/public.sdk/source/vst
+		${VST3SDK_HOME}/public.sdk/source/vst2.x
+		${VST3SDK_HOME}/vstgui4
+		${VST3SDK_HOME}/vstgui4/vstgui
+	)
+endif()
+
+
+message(" FindAsiosdk VST3SDK_INCLUDE_DIR : ${VST3SDK_INCLUDE_DIR}")
+
+# handle the QUIETLY and REQUIRED arguments and set VST3SDK_FOUND to TRUE if 
+# all listed variables are TRUE
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(VST3SDK DEFAULT_MSG VST3SDK_HOME VST3SDK_INCLUDE_DIR)
+
+MARK_AS_ADVANCED(
+    VST3SDK_HOME VST3SDK_INCLUDE_DIR
+)
---
+++ ports/vst3sdk/portfile.cmake
@@ -0,0 +1,109 @@
+
+# Specifies if the port install should fail immediately given a condition
+vcpkg_fail_port_install(MESSAGE "vst3sdk currently only supports Windows platforms" ON_TARGET "Linux" "OSX")
+
+set(BUILD_TOOLS "OFF")
+if("tools" IN_LIST FEATURES)
+	message(STATUS "Requested building of tools.")
+    set(BUILD_TOOLS "ON")
+endif()
+
+set(BUILD_EXAMPLES "OFF")
+if("examples" IN_LIST FEATURES)
+	message(STATUS "Requested building of examples.")
+    set(BUILD_EXAMPLES "ON")
+endif()
+
+set(USE_VST3SDK_V366 "OFF")
+if("legacy" IN_LIST FEATURES)
+	message(STATUS "Requested building of vst3sdk version 3.6.6.")
+	message(STATUS "[tools] and [examples] features will be turned off.")
+    set(USE_VST3SDK_V366 "ON")
+    set(BUILD_TOOLS "OFF")
+    set(BUILD_EXAMPLES "OFF")
+endif()
+
+
+if(USE_VST3SDK_V366)
+	message(STATUS "Using previous version of vst3sdk that integrates vst2sdk.")
+
+	vcpkg_download_distfile(ARCHIVE
+		URLS "https://www.steinberg.net/sdk_downloads/vstsdk366_27_06_2016_build_61.zip"
+		FILENAME "vstsdk366_27_06_2016_build_61.zip"
+		SHA512 79b186dae42b7a0c2f6ffe4bfeeed68267bfc2f4ce58475e0e84b7b0d8e113de22d95180fc3df6179569bafc5a1f48e8008690d655fc2b12609fd5150f591e4b
+		
+	)
+else()
+	message(STATUS "Using lastest version of vst3sdk that contains vst2sdk wrappers.")
+	vcpkg_download_distfile(archive
+		urls "https://download.steinberg.net/sdk_downloads/vstsdk3612_03_12_2018_build_67.zip"
+		filename "vstsdk3612_03_12_2018_build_67.zip"
+		sha512 7f39bf01c055c6ae11f8d982222b511446b9dd9d04ba41344d0d9692ecf491f1e994e8e7e432fef3846eb768ba88a537be46a0b2cd6b9793b72f211ea5c67630
+	)
+endif()
+
+
+
+
+vcpkg_extract_source_archive_ex(
+	OUT_SOURCE_PATH SOURCE_PATH
+	NO_REMOVE_ONE_LEVEL
+	ARCHIVE ${ARCHIVE}
+	REF ${VERSION}
+)
+
+
+message(STATUS "Building ${TARGET_TRIPLET}")
+
+if(NOT USE_VST3SDK_V366)
+	vcpkg_execute_required_process(
+		COMMAND ${SOURCE_PATH}/VST_SDK/copy_vst2_to_vst3_sdk.bat
+		WORKING_DIRECTORY ${SOURCE_PATH}/VST_SDK
+		LOGNAME copy-vst2-to-vst3-${TARGET_TRIPLET}
+	)
+endif()
+
+
+if(BUILD_TOOLS)
+	vcpkg_configure_cmake(
+		SOURCE_PATH ${SOURCE_PATH}/VST_SDK/VST3_SDK
+		PREFER_NINJA
+		OPTIONS
+			-DSMTG_ADD_VST3_HOSTING_SAMPLES=OFF
+			-DSMTG_ADD_VST3_PLUGINS_SAMPLES=OFF
+			-DSMTG_VSTGUI_TOOLS=${VSTGUI_HAVE_TOOLS}
+	)
+
+	vcpkg_build_cmake()
+
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/vst3sdk)
+    file(INSTALL ${CMAKE_BINARY_DIR}/bin/Release/ImageStitcher.exe DESTINATION ${CURRENT_PACKAGES_DIR}/tools/vst3sdk)
+    file(INSTALL ${CMAKE_BINARY_DIR}/bin/Release/uidesccompressor.exe DESTINATION ${CURRENT_PACKAGES_DIR}/tools/vst3sdk)
+    vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/vst3sdk)
+endif()
+
+
+if(USE_VST3SDK_V366)
+	file(INSTALL "${SOURCE_PATH}/VST3 SDK/base" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
+	file(INSTALL "${SOURCE_PATH}/VST3 SDK/pluginterfaces" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
+	file(INSTALL "${SOURCE_PATH}/VST3 SDK/public.sdk" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
+	file(INSTALL "${SOURCE_PATH}/VST3 SDK/vstgui.sf" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
+	file(INSTALL "${SOURCE_PATH}/VST3 SDK/vstgui4" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
+	file(INSTALL "${SOURCE_PATH}/VST3 SDK/doc" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+	file(INSTALL "${SOURCE_PATH}/VST3 SDK/bin" DESTINATION ${CURRENT_PACKAGES_DIR}/bin/${PORT})
+	file(INSTALL "${SOURCE_PATH}/VST3 SDK/doc/VST3_License_Agreement.rtf" DESTINATION ${CURRENT_PACKAGES_DIR}/share/vst3sdk RENAME copyright)
+else()
+	file(INSTALL "${SOURCE_PATH}/VST_SDK/VST3_SDK/" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
+	file(INSTALL "${SOURCE_PATH}/VST_SDK/VST3_SDK/index.html" DESTINATION ${CURRENT_PACKAGES_DIR}/share/vst3sdk)
+	file(INSTALL "${SOURCE_PATH}/VST_SDK/VST3_SDK/README.md" DESTINATION ${CURRENT_PACKAGES_DIR}/share/vst3sdk)
+	file(INSTALL "${SOURCE_PATH}/VST_SDK/VST3_SDK/LICENSE.txt" DESTINATION ${CURRENT_PACKAGES_DIR}/share/vst3sdk RENAME copyright)
+	file(INSTALL "${SOURCE_PATH}/VST_SDK/VST3_SDK/VST3_License_Agreement.pdf" DESTINATION ${CURRENT_PACKAGES_DIR}/share/vst3sdk)
+	file(INSTALL "${SOURCE_PATH}/VST_SDK/VST3_SDK/VST3_Usage_Guidelines.pdf" DESTINATION ${CURRENT_PACKAGES_DIR}/share/vst3sdk)
+
+endif()
+
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/Findvst3sdk.cmake" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+
