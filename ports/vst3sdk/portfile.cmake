7acc6608f5f589c97cd801352303034a39da818d
Michael Mattsson <michael.mattsson@konicaminolta.com>
2020-01-27 13:39:52 -0500

    small changes to pull latest legacy vst3sdk when using [legacy] feature

--- ports/vst3sdk/portfile.cmake
+++ ports/vst3sdk/portfile.cmake
@@ -1,4 +1,3 @@
-
 # Specifies if the port install should fail immediately given a condition
 vcpkg_fail_port_install(MESSAGE "vst3sdk currently only supports Windows platforms" ON_TARGET "Linux" "OSX")
 
@@ -14,25 +13,29 @@ if("examples" IN_LIST FEATURES)
     set(BUILD_EXAMPLES "ON")
 endif()
 
-set(USE_VST3SDK_V366 "OFF")
+set(USE_VST3SDK_V367 "OFF")
 if("legacy" IN_LIST FEATURES)
 	message(STATUS "Requested building of vst3sdk version 3.6.6.")
 	message(STATUS "[tools] and [examples] features will be turned off.")
-    set(USE_VST3SDK_V366 "ON")
+    set(USE_VST3SDK_V367 "ON")
     set(BUILD_TOOLS "OFF")
     set(BUILD_EXAMPLES "OFF")
 endif()
 
-
-if(USE_VST3SDK_V366)
+	#vcpkg_download_distfile(ARCHIVE
+		#URLS "https://www.steinberg.net/sdk_downloads/vstsdk366_27_06_2016_build_61.zip"
+		#FILENAME "vstsdk366_27_06_2016_build_61.zip"
+		#SHA512 79b186dae42b7a0c2f6ffe4bfeeed68267bfc2f4ce58475e0e84b7b0d8e113de22d95180fc3df6179569bafc5a1f48e8008690d655fc2b12609fd5150f591e4b
+	#)
+if(USE_VST3SDK_V367)
 	message(STATUS "Using previous version of vst3sdk that integrates vst2sdk.")
 
 	vcpkg_download_distfile(ARCHIVE
-		URLS "https://www.steinberg.net/sdk_downloads/vstsdk366_27_06_2016_build_61.zip"
-		FILENAME "vstsdk366_27_06_2016_build_61.zip"
-		SHA512 79b186dae42b7a0c2f6ffe4bfeeed68267bfc2f4ce58475e0e84b7b0d8e113de22d95180fc3df6179569bafc5a1f48e8008690d655fc2b12609fd5150f591e4b
-		
+		URLS "https://www.steinberg.net/sdk_downloads/vstsdk367_03_03_2017_build_352.zip"
+		FILENAME "vstsdk367_03_03_2017_build_352.zip"
+		SHA512 54ed5101c4b1b07f2341c5c4440223f4de08f9a12b08cc335c5917297db74d1474ba84f06d0120f0b7ae074d9ad776250396269ac7617b69bbab7937c752e098
 	)
+
 else()
 	message(STATUS "Using lastest version of vst3sdk that contains vst2sdk wrappers.")
 	vcpkg_download_distfile(archive
@@ -41,8 +44,6 @@ else()
 		sha512 7f39bf01c055c6ae11f8d982222b511446b9dd9d04ba41344d0d9692ecf491f1e994e8e7e432fef3846eb768ba88a537be46a0b2cd6b9793b72f211ea5c67630
 	)
 endif()
-
-
 
 
 vcpkg_extract_source_archive_ex(
@@ -55,12 +56,15 @@ vcpkg_extract_source_archive_ex(
 
 message(STATUS "Building ${TARGET_TRIPLET}")
 
-if(NOT USE_VST3SDK_V366)
-	vcpkg_execute_required_process(
-		COMMAND ${SOURCE_PATH}/VST_SDK/copy_vst2_to_vst3_sdk.bat
-		WORKING_DIRECTORY ${SOURCE_PATH}/VST_SDK
-		LOGNAME copy-vst2-to-vst3-${TARGET_TRIPLET}
-	)
+if(NOT USE_VST3SDK_V367)
+	# vcpkg_execute_required_process(
+		# COMMAND ${SOURCE_PATH}/VST_SDK/copy_vst2_to_vst3_sdk.bat
+		# WORKING_DIRECTORY ${SOURCE_PATH}/VST_SDK
+		# LOGNAME copy-vst2-to-vst3-${TARGET_TRIPLET}
+	# )
+	
+	copy(FILE ${SOURCE_PATH}/VST_SDK/VST2_SDK ${SOURCE_PATH}/VST_SDK/VST3_SDK)
+	
 endif()
 
 
@@ -83,15 +87,20 @@ if(BUILD_TOOLS)
 endif()
 
 
-if(USE_VST3SDK_V366)
-	file(INSTALL "${SOURCE_PATH}/VST3 SDK/base" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
-	file(INSTALL "${SOURCE_PATH}/VST3 SDK/pluginterfaces" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
-	file(INSTALL "${SOURCE_PATH}/VST3 SDK/public.sdk" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
-	file(INSTALL "${SOURCE_PATH}/VST3 SDK/vstgui.sf" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
-	file(INSTALL "${SOURCE_PATH}/VST3 SDK/vstgui4" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
-	file(INSTALL "${SOURCE_PATH}/VST3 SDK/doc" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-	file(INSTALL "${SOURCE_PATH}/VST3 SDK/bin" DESTINATION ${CURRENT_PACKAGES_DIR}/bin/${PORT})
-	file(INSTALL "${SOURCE_PATH}/VST3 SDK/doc/VST3_License_Agreement.rtf" DESTINATION ${CURRENT_PACKAGES_DIR}/share/vst3sdk RENAME copyright)
+if(USE_VST3SDK_V367)
+	file(INSTALL "${SOURCE_PATH}/VST_SDK/VST3_SDK/" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
+	file(INSTALL "${SOURCE_PATH}/VST_SDK/VST3_SDK/index.html" DESTINATION ${CURRENT_PACKAGES_DIR}/share/vst3sdk)
+	file(INSTALL "${SOURCE_PATH}/VST_SDK/VST3_SDK/README.md" DESTINATION ${CURRENT_PACKAGES_DIR}/share/vst3sdk)
+	file(INSTALL "${SOURCE_PATH}/VST_SDK/VST3_SDK/LICENSE.txt" DESTINATION ${CURRENT_PACKAGES_DIR}/share/vst3sdk RENAME copyright)
+
+	# file(INSTALL "${SOURCE_PATH}/VST3 SDK/base" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
+	# file(INSTALL "${SOURCE_PATH}/VST3 SDK/pluginterfaces" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
+	# file(INSTALL "${SOURCE_PATH}/VST3 SDK/public.sdk" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
+	# file(INSTALL "${SOURCE_PATH}/VST3 SDK/vstgui.sf" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
+	# file(INSTALL "${SOURCE_PATH}/VST3 SDK/vstgui4" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
+	# file(INSTALL "${SOURCE_PATH}/VST3 SDK/doc" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+	# file(INSTALL "${SOURCE_PATH}/VST3 SDK/bin" DESTINATION ${CURRENT_PACKAGES_DIR}/bin/${PORT})
+	# file(INSTALL "${SOURCE_PATH}/VST3 SDK/doc/VST3_License_Agreement.rtf" DESTINATION ${CURRENT_PACKAGES_DIR}/share/vst3sdk RENAME copyright)
 else()
 	file(INSTALL "${SOURCE_PATH}/VST_SDK/VST3_SDK/" DESTINATION ${CURRENT_PACKAGES_DIR}/include/vst3sdk)
 	file(INSTALL "${SOURCE_PATH}/VST_SDK/VST3_SDK/index.html" DESTINATION ${CURRENT_PACKAGES_DIR}/share/vst3sdk)
